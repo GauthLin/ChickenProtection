a93ca9b55a3d83e0e16c4f91b5b7cafcea2136dd
Linard <linard.gauthier@gmail.com>
2018-11-11 13:55:52 +0100

    Add config file to save parameter of the app

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request
+import configparser
+import os
+
+configFileName = 'config.ini'
 
 app = Flask(__name__)
-app.add_url_rule('/node_modules/<path:filename>', endpoint='node_modules',
-                 view_func=app.send_static_file)
 
 @app.route("/")
 def index():
@@ -12,5 +14,32 @@ def index():
 def save():
     return "saved !"
 
+def save_config():
+    with open(configFileName, 'w') as f:
+        config.write(f)
+
+def read_config_file():
+    config.read(configFileName)
+    if not os.path.isfile(configFileName):
+        config.add_section('common')
+        config.set('common', 'Mode', '1')
+        config.set('common', 'Debug', 'false')
+
+        config.add_section('auto')
+        config.set('auto', 'opening_mode', '1')
+        config.set('auto', 'opening_correction', '0')
+        config.set('auto', 'opening_time', '')
+        config.set('auto', 'closing_mode', '1')
+        config.set('auto', 'closing_correction', '0')
+        config.set('auto', 'closing_time', '')
+
+        config.add_section('manual')
+        config.set('manual', 'manual_mode', '1')
+
+        save_config()
+
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=80, debug=True)
+    config = configparser.ConfigParser()
+    read_config_file()
+
+    app.run(host='0.0.0.0', port=80, debug=config.getboolean('common', 'debug'))
