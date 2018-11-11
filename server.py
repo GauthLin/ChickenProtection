acd7f579dcb9c930ec3e19224162f5b176b76ab1
Linard <linard.gauthier@gmail.com>
2018-11-11 12:43:28 +0100

    Finish GUI

--- server.py
+++ server.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 app.add_url_rule('/node_modules/<path:filename>', endpoint='node_modules',
                  view_func=app.send_static_file)
 
 @app.route("/")
-def hello():
+def index():
 	return render_template("index.html")
+
+@app.route("/save", methods=['POST'])
+def save():
+    return "saved !"
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=80, debug=True)
--- static/js/app.js
+++ static/js/app.js
@@ -1,27 +1,60 @@
 $(function(){
-    var $sectionAuto = $("#ModeAuto");
-    var $sectionManu = $("#ModeManu");
-    var $SelectionMode = $('select#inputGroupSelectMode');
-
     $(document).ready(function() {
+        var $sectionAuto = $("#ModeAuto");
+        var $sectionManu = $("#ModeManu");
+        var $SelectionMode = $('select#ProtectionMode');
+        var $OpeningTimeCorrectionSection = $('#OpeningTimeCorrectionSection')
+        var $OpeningTimeSection = $('#OpeningTimeSection')
+        var $OpeningMode = $('#OpeningMode')
+        var $FormReset = $('#FormReset')
+        var $ClosingMode = $('#ClosingMode')
+        var $ClosingTimeSection = $('#ClosingTimeSection')
+        var $ClosingTimeCorrectionSection = $('#ClosingTimeCorrectionSection')
+
         DisplaySections();
-    });
+        DisplayTimeSections($OpeningMode, $OpeningTimeCorrectionSection, $OpeningTimeSection);
+        DisplayTimeSections($ClosingMode, $ClosingTimeCorrectionSection, $ClosingTimeSection);
 
-    $SelectionMode.on('change', function() {
-        DisplaySections();
-    });
+        $SelectionMode.on('change', function() {
+            DisplaySections();
+        });
 
-    function DisplaySections() {
-        var value = $SelectionMode.val();
+        $OpeningMode.on('change', function() {
+            DisplayTimeSections($OpeningMode, $OpeningTimeCorrectionSection, $OpeningTimeSection);
+        });
 
-        if (value === "auto")
-        {
-            $sectionManu.hide();
-            $sectionAuto.show();
+        $ClosingMode.on('change', function() {
+            DisplayTimeSections($ClosingMode, $ClosingTimeCorrectionSection, $ClosingTimeSection);
+        });
+
+        $FormReset.on('click', function() {
+            setTimeout(DisplaySections, 1);
+        });
+
+        function DisplaySections() {
+            var value = $SelectionMode.val();
+
+            if (value === "auto") {
+                $sectionManu.hide();
+                $sectionAuto.show();
+                DisplayTimeSections($OpeningMode, $OpeningTimeCorrectionSection, $OpeningTimeSection);
+                DisplayTimeSections($ClosingMode, $ClosingTimeCorrectionSection, $ClosingTimeSection);
+            } else {
+                $sectionManu.show();
+                $sectionAuto.hide();
+            }
         }
-        else {
-            $sectionManu.show();
-            $sectionAuto.hide();
+
+        function DisplayTimeSections($mode, $correctionSection, $timeSection) {
+            var value = $mode.val();
+
+            if (value == 1) {
+                $correctionSection.show();
+                $timeSection.hide();
+            } else {
+                $correctionSection.hide();
+                $timeSection.show();
+            }
         }
-    }
+    });
 });
