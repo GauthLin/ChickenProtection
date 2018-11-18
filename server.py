a595b9464b7dac462ef02f13a25a91e58572ef09
Linard <linard.gauthier@gmail.com>
2018-11-18 18:14:29 +0100

    Create application to open and close the door and finish GUI

--- server.py
+++ server.py
@@ -1,14 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 from flask import Flask, render_template, request
+from threading import Thread
+from enum import Enum
 import configparser
 import os
+import time
+import datetime
+from astral import Astral
 
 configFileName = 'config.ini'
-app = Flask(__name__)
+webserver = Flask(__name__)
+sunrise = sunset = ''
 
-@app.route("/")
+
+class ProtectionMode(Enum):
+    Auto = 1
+    Manu = 2
+
+
+class App(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        self.a = Astral()
+        self.city = self.a['Brussels']
+
+    def run(self):
+        while True:
+            current_time = datetime.datetime.now()
+            # Si en mode auto
+            if config.get('common', 'mode') == ProtectionMode.Auto.name:
+                sun = self.city.sun(date=datetime.datetime.now(), local=True)
+                sunrise = sun['sunrise']
+                sunset = sun['sunset']
+                correction_opening = config.getint('auto', 'opening_correction')
+                correction_closing = config.getint('auto', 'closing_correction')
+                opening_mode = config.getint('auto', 'opening_mode')
+                closing_mode = config.getint('auto', 'closing_mode')
+                opening_time = str.split(config.get('auto', 'opening_time'), ':')
+                closing_time = str.split(config.get('auto', 'closing_time'), ':')
+
+                new_sunrise = sunrise + datetime.timedelta(minutes=correction_opening)
+                new_sunset = sunset + datetime.timedelta(minutes=correction_closing)
+
+                # if closing_mode == 1:
+                if ((closing_mode == 1 and current_time.hour >= new_sunset.hour and current_time.minute >= new_sunset.minute)
+                        or (closing_mode == 2 and current_time.hour >= int(closing_time[0]) and current_time.minute >= int(closing_time[1]))):
+                    self.close_door()
+                elif ((opening_mode == 1 and current_time.hour >= new_sunrise.hour and current_time.minute >= new_sunrise.minute)
+                        or (opening_mode == 2 and current_time.hour >= int(opening_time[0]) and current_time.minute >= int(opening_time[1]))):
+                    self.open_door()
+                else:
+                    self.close_door()
+
+            time.sleep(60)
+
+    def close_door(self):
+        print('Time to close the door')
+
+    def open_door(self):
+        print('Time to open the door')
+
+
+@webserver.route("/")
 def index():
     dict = {
         'ProtectionMode': config.get('common', "mode"),
+        'OpeningMode': config.get('auto', "opening_mode"),
         'OpeningTimeCorrection': config.get('auto', "opening_correction"),
         'OpeningTime': config.get('auto', "opening_time"),
         'ClosingMode': config.get('auto', "closing_mode"),
@@ -18,39 +77,42 @@ def index():
     }
     return render_template("index.html", config=dict)
 
-@app.route("/save", methods=['POST'])
+
+@webserver.route("/save", methods=['POST'])
 def save():
     config.set('common', 'mode', request.form["ProtectionMode"])
 
     config.set('auto', 'opening_mode', request.form['OpeningMode'])
     config.set('auto', 'opening_correction', request.form['OpeningTimeCorrection'])
-    config.set('auto', 'opening_time', request.form['OpeningTime'])
+    config.set('auto', 'opening_time', request.form['OpeningTime'] if request.form['OpeningTime'] != '' else '00:00')
     config.set('auto', 'closing_mode', request.form['ClosingMode'])
     config.set('auto', 'closing_correction', request.form['ClosingTimeCorrection'])
-    config.set('auto', 'closing_time', request.form['ClosingTime'])
+    config.set('auto', 'closing_time', request.form['ClosingTime'] if request.form['ClosingTime'] != '' else '23:59')
 
     config.set('manual', 'manual_mode', request.form['ForceDoorStatus'])
     save_config()
-    return "saved !"
+    return "saved"
+
 
 def save_config():
     with open(configFileName, 'w') as f:
         config.write(f)
 
+
 def read_config_file():
     config.read(configFileName)
     if not os.path.isfile(configFileName):
         config.add_section('common')
-        config.set('common', 'Mode', '1')
+        config.set('common', 'Mode', ProtectionMode.Auto.name)
         config.set('common', 'Debug', 'false')
 
         config.add_section('auto')
         config.set('auto', 'opening_mode', '1')
         config.set('auto', 'opening_correction', '0')
-        config.set('auto', 'opening_time', '')
+        config.set('auto', 'opening_time', '00:00')
         config.set('auto', 'closing_mode', '1')
         config.set('auto', 'closing_correction', '0')
-        config.set('auto', 'closing_time', '')
+        config.set('auto', 'closing_time', '23:59')
 
         config.add_section('manual')
         config.set('manual', 'manual_mode', '1')
@@ -62,4 +124,8 @@ if __name__ == "__main__":
     config = configparser.ConfigParser()
     read_config_file()
 
-    app.run(host='0.0.0.0', port=80, debug=config.getboolean('common', 'debug'))
+    # Start the thread parallèle
+    app = App()
+    app.start()
+
+    webserver.run(host='0.0.0.0', port=80, debug=config.getboolean('common', 'debug'))
--- static/js/app.js
+++ static/js/app.js
@@ -1,81 +1,84 @@
 $(function(){
-    $(document).ready(function() {
-        var $sectionAuto = $("#ModeAuto");
-        var $sectionManu = $("#ModeManu");
-        var $SelectionMode = $('select#ProtectionMode');
-        var $OpeningTimeCorrectionSection = $('#OpeningTimeCorrectionSection')
-        var $OpeningTimeSection = $('#OpeningTimeSection')
-        var $OpeningMode = $('#OpeningMode')
-        var $FormReset = $('#FormReset')
-        var $ClosingMode = $('#ClosingMode')
-        var $ClosingTimeSection = $('#ClosingTimeSection')
-        var $ClosingTimeCorrectionSection = $('#ClosingTimeCorrectionSection')
+    var $sectionAuto = $("#ModeAuto");
+    var $sectionManu = $("#ModeManu");
+    var $SelectionMode = $('select#ProtectionMode');
+    var $OpeningTimeCorrectionSection = $('#OpeningTimeCorrectionSection')
+    var $OpeningTimeSection = $('#OpeningTimeSection')
+    var $OpeningMode = $('#OpeningMode')
+    var $FormReset = $('#FormReset')
+    var $ClosingMode = $('#ClosingMode')
+    var $ClosingTimeSection = $('#ClosingTimeSection')
+    var $ClosingTimeCorrectionSection = $('#ClosingTimeCorrectionSection')
 
+    DisplaySections();
+    DisplayTimeSections($OpeningMode, $OpeningTimeCorrectionSection, $OpeningTimeSection);
+    DisplayTimeSections($ClosingMode, $ClosingTimeCorrectionSection, $ClosingTimeSection);
+
+    $SelectionMode.on('change', function() {
         DisplaySections();
-        DisplayTimeSections($OpeningMode, $OpeningTimeCorrectionSection, $OpeningTimeSection);
-        DisplayTimeSections($ClosingMode, $ClosingTimeCorrectionSection, $ClosingTimeSection);
-
-        $SelectionMode.on('change', function() {
-            DisplaySections();
-        });
-
-        $OpeningMode.on('change', function() {
-            DisplayTimeSections($OpeningMode, $OpeningTimeCorrectionSection, $OpeningTimeSection);
-        });
-
-        $ClosingMode.on('change', function() {
-            DisplayTimeSections($ClosingMode, $ClosingTimeCorrectionSection, $ClosingTimeSection);
-        });
-
-        $FormReset.on('click', function() {
-            setTimeout(DisplaySections, 1);
-        });
-
-        $('form').on('submit', function(e) {
-            var $form = $(this);
-            var action = $form.attr('action');
-
-            //$.ajax({
-            //    type: "POST",
-            //    url: action,
-            //    data: $form.serialize(),
-            //    success: function(data) {
-            //        $.confirm({
-            //            title: 'Hey there',
-            //            content: 'Hello some text',
-            //            type: 'green',
-            //            typeAnimated: true
-            //        });
-            //    }
-            //});
-
-            //e.preventDefault();
-        });
-
-        function DisplaySections() {
-            var value = $SelectionMode.val();
-
-            if (value === "auto") {
-                $sectionManu.hide();
-                $sectionAuto.show();
-                DisplayTimeSections($OpeningMode, $OpeningTimeCorrectionSection, $OpeningTimeSection);
-                DisplayTimeSections($ClosingMode, $ClosingTimeCorrectionSection, $ClosingTimeSection);
-            } else {
-                $sectionManu.show();
-                $sectionAuto.hide();
-            }
-        }
-
-        function DisplayTimeSections($mode, $correctionSection, $timeSection) {
-            var value = $mode.val();
-
-            if (value == 1) {
-                $correctionSection.show();
-                $timeSection.hide();
-            } else {
-                $correctionSection.hide();
-                $timeSection.show();
-            }
-        }
     });
+
+    $OpeningMode.on('change', function() {
+        DisplayTimeSections($OpeningMode, $OpeningTimeCorrectionSection, $OpeningTimeSection);
+    });
+
+    $ClosingMode.on('change', function() {
+        DisplayTimeSections($ClosingMode, $ClosingTimeCorrectionSection, $ClosingTimeSection);
+    });
+
+    $FormReset.on('click', function() {
+        setTimeout(DisplaySections, 1);
+    });
+
+    $('form').on('submit', function(e) {
+        var $form = $(this);
+        var action = $form.attr('action');
+
+        $.ajax({
+            type: "POST",
+            url: action,
+            data: $form.serialize(),
+            success: function(data) {
+                $.confirm({
+                    title: 'Paramètres enregistrés',
+                    content: 'Les paramètres ont bien été enregistrés !',
+                    type: 'green',
+                    typeAnimated: true,
+                    buttons: {
+                        ok: {
+                            text: "Ok"
+                        }
+                    }
+                });
+            }
+        });
+
+        e.preventDefault();
+    });
+
+    function DisplaySections() {
+        var value = $SelectionMode.val();
+
+        if (value === "Auto") {
+            $sectionManu.hide();
+            $sectionAuto.show();
+            DisplayTimeSections($OpeningMode, $OpeningTimeCorrectionSection, $OpeningTimeSection);
+            DisplayTimeSections($ClosingMode, $ClosingTimeCorrectionSection, $ClosingTimeSection);
+        } else {
+            $sectionManu.show();
+            $sectionAuto.hide();
+        }
+    }
+
+    function DisplayTimeSections($mode, $correctionSection, $timeSection) {
+        var value = $mode.val();
+
+        if (value == 1) {
+            $correctionSection.show();
+            $timeSection.hide();
+        } else {
+            $correctionSection.hide();
+            $timeSection.show();
+        }
+    }
 });
